2caa8fa2288e270d6a384349a5be7757be8b2660
riyanurdiansyah <riyanurdiansyah@gmail.com>
2022-10-10 13:37:11 +0700

    publish branch dev

--- lib/src/presentation/pages/profile/sertifikat/widget/sertifikat_kelulusan_tab.dart
+++ lib/src/presentation/pages/profile/sertifikat/widget/sertifikat_kelulusan_tab.dart
@@ -120,7 +120,8 @@ class SertifikatKelulusanTab extends StatelessWidget {
                       (i) => SertifikatCard(
                         sertif: sertif[i],
                         onTapUnduh: () => _sC.fnCheckPermission(i),
-                        onTapShare: () => _sC.fnSharedCertificate(),
+                        onTapShare: () => _sC.fnSharedCertificate(
+                            'Saya telah menyelesaikan dan lulus proyek akhir dalam kursus ${sertif[i].courseName} yang diselenggarakan oleh Arkademi. Cek link berikut ini: ${sertif[i].certificateUrl}'),
                         onTapShareLinkedin: () async {
                           await AppFirebaseAnalyticsService()
                               .addLog("click_share_linkedin_kelulusan");
